41d68813c5c8d6e5a403efbe4ae8a514d03d587e
Binit Rai <binitcse@gmail.com>
2019-12-22 00:33:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Login from "./containers/Login/Login";
 import Dashboard from "./containers/Dashboard/Dashboard";
+import NotFound from "./components/NotFound";
 import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import PrivateRoute from "./privateRoute";
@@ -12,7 +13,8 @@ class App extends Component {
           <Layout>
             <Switch>
               <Route path="/" exact component={Login} />
-              <PrivateRoute path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <Route component={NotFound} />
             </Switch>
           </Layout>
         </div>
