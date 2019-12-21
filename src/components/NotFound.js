4f8c1883262b399c5104d943be1b38bca4f94583
Binit Rai <binitcse@gmail.com>
2019-12-22 00:36:05 +0530

    Update NotFound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function NotFound(props) {
     return (
-        <div><h1>404</h1></div>
+        <div className={classes.NotFound}><h1>404</h1></div>
     )
 }
 export default NotFound;
